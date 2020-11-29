822dfc37ace602df7e1e8f884e3c99b18d61949a
ruan-fachini <ruan.fachini@performait.com>
2020-11-29 10:16:20 -0300

    first

---
+++ WebApiCourse.API/Controllers/ProfessorController.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiCourse.Domain.Models;
+
+namespace WebApiCourse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorController : ControllerBase
+    {
+        public ProfessorController()
+        {
+            
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet("{id:int}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPost]
+        public IActionResult Post([FromBody] Professor model)
+        {
+            return Created("", model);
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPut("{id:int}")]
+        public IActionResult Post([FromRoute] int id, [FromBody] Professor model)
+        {
+            return NoContent();
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpPut("{id:int}")]
+        public IActionResult Patch([FromRoute] int id, [FromBody] Professor model)
+        {
+            return NoContent();
+        }
+        
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpDelete("{id}:int")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ WebApiCourse.Domain/Models/AlunoDisciplina.cs
@@ -0,0 +1,19 @@
+﻿
+namespace WebApiCourse.Domain.Models
+{
+    public class AlunoDisciplina
+    {
+        public AlunoDisciplina() { }
+
+        public AlunoDisciplina(int disciplinaId, int alunoId)
+        {
+            this.DisciplinaId = disciplinaId;
+            this.AlunoId = alunoId;
+        }
+
+        public int DisciplinaId { get; set; }
+        public Disciplina Disciplina { get; set; }
+        public int AlunoId { get; set; }
+        public Aluno Aluno { get; set; }
+    }
+}
